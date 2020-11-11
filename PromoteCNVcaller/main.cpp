b84903e2ec6838f781358beeec8046bd2a9852b1
Streambuns <flyingpig@nwsuaf.edu.cn>
2020-11-11 13:59:33 +0800

    Initial Commit

---
+++ PromoteCNVcaller/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PromoteCNVcaller
+//
+//  Created by 赵桐 on 2020/11/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
